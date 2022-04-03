4e5e123afa1c6017f44ab6556ebad6f134c92a34
Lailson Sobral <lailsonlm@hotmail.com>
2022-04-03 15:54:47 -0300

    added comunidev project in portfolio

--- script/swiper.js
+++ script/swiper.js
@@ -10,6 +10,7 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
+const p7 = document.querySelector('#portfolio7')
 const p0 = document.querySelector('#portfolio0')
 const p1 = document.querySelector('#portfolio1')
 const p2 = document.querySelector('#portfolio2')
@@ -18,37 +19,42 @@ const p4 = document.querySelector('#portfolio4')
 const p5 = document.querySelector('#portfolio5')
 const p6 = document.querySelector('#portfolio6')
 
-p0.addEventListener('click', (e) => {
+p7.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(1, 0);
 })
 
-p1.addEventListener('click', (e) => {
+p0.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(2, 0);
 })
 
-p2.addEventListener('click', (e) => {
+p1.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(3, 0);
 })
 
-p3.addEventListener('click', (e) => {
+p2.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(4, 0);
 })
 
-p4.addEventListener('click', (e) => {
+p3.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(5, 0);
 })
 
-p5.addEventListener('click', (e) => {
+p4.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(6, 0);
 })
 
-p6.addEventListener('click', (e) => {
+p5.addEventListener('click', (e) => {
   e.preventDefault();
   swiper.slideTo(7, 0);
+})
+
+p6.addEventListener('click', (e) => {
+  e.preventDefault();
+  swiper.slideTo(8, 0);
 })
