1420745eb6db215b4e5b4db44b5b47dcf72c59c3
Lailson Sobral <lailsonlm@hotmail.com>
2022-05-26 17:40:42 -0300

    added contact form

--- script/dataAnime.js
+++ script/dataAnime.js
@@ -14,10 +14,11 @@ const debounce = function(func, wait, immediate) {
 }
 
 const target = document.querySelectorAll('[data-anime]')
+const targetRelative = document.querySelectorAll('[data-anime-relative]')
 const animationClass = 'animate'
 
 function animeScroll() {
-  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);  
   target.forEach(function(element) {
       if(windowTop > element.offsetTop) {
           element.classList.add(animationClass)
@@ -29,8 +30,28 @@ function animeScroll() {
 
 animeScroll();
 
+function animeScrollRelative() {
+  const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+ 
+  targetRelative.forEach(function(element) {
+      if(windowTop > element.offsetParent.offsetTop) {
+          element.classList.add(animationClass)
+      } else {
+          element.classList.remove(animationClass)
+      }
+  })
+}
+
+animeScrollRelative();
+
 if(target.length) {
   window.addEventListener('scroll', debounce(function() {
       animeScroll()
+  }, 100));
+}
+
+if(targetRelative.length) {
+  window.addEventListener('scroll', debounce(function() {
+      animeScrollRelative()
   }, 100));
 }
